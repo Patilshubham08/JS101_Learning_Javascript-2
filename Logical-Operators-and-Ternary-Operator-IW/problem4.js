e152da679a17242e72dac91f33ce264fcdb9264c
Your Name <shubhamraj.patil@gmail.com>
2022-10-10 17:22:23 +0530

    Conditional Statements problems

---
+++ Logical-Operators-and-Ternary-Operator-IW/problem4.js
@@ -0,0 +1,9 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a=50;
+let b=20;
+let c=25;
+
+a>b && a>c ? console.log("a is greater") 
+  : b>a && b>c ? console.log("b is greater") 
+  : console.log("c is greater");
