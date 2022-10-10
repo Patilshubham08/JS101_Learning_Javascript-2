f7c83fdac5ea9fa79ff0d50901e58799f0e183cf
Your Name <shubhamraj.patil@gmail.com>
2022-10-10 16:52:44 +0530

    Variable Problems

---
+++ Introduction-to-JS-Variables-and-Data-Types/problem2.js
@@ -0,0 +1,19 @@
+//Problem 2
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+var name="Shubham";
+
+console.log("My name is :",name);
+
+var fathers_name="Bhupal"
+
+console.log("fathers_name is :",fathers_name);
+
+var mothers_name="Rekhatai";
+
+console.log("mothers_name is :",mothers_name)
---
+++ Introduction-to-JS-Variables-and-Data-Types/problem4.js
@@ -0,0 +1,35 @@
+//Problem 4
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+console.log("👍👍👍👍 REPORT CARD 👍👍👍👍👍");
+console.log("            ")
+
+console.log("Student Name :","Shubham Patil");
+console.log("            ")
+
+console.log("School Name :","Masai School");
+console.log("            ")
+
+console.log("Grade :","A")
+console.log("            ")
+
+console.log("Roll no. :","0419");
+console.log("            ")
+
+let math=80;
+let english=75;
+let history=85;
+
+console.log("math :",math);
+console.log("english :",english);
+console.log("history :",history);
+
+
+
+
+
+
+
+
+
